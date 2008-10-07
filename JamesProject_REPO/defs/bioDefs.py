16e88f4eca26218fed7d5716629b4974c9f08145
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2008-10-07 18:31:47 +0000

    initial commit
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@12 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

---
+++ JamesProject_REPO/defs/bioDefs.py
@@ -0,0 +1,80 @@
+def fastaFileToBioSeqDict(pathToFastaFile, Alphabet='IUPACAmbiguousDNA', splitOn=None, indexAsName=0):
+    from Bio.Alphabet.IUPAC import Alphabet
+    from Bio import SeqIO
+    #  Populate a Dict with Seq objs for Anopheles boundary seqs
+    #  What follows directly is a klugde to get all my seqDict vals to have an IUPAC alphabet
+    listOfSeqs = list(SeqIO.parse(open(pathToFastaFile, "rU"), "fasta"))
+    for record in listOfSeqs :
+        record.seq.alphabet = Alphabet
+
+    dictOfSeqs = SeqIO.to_dict(listOfSeqs, key_function = lambda rec : rec.description.split(splitOn)[indexAsName])
+
+    return dictOfSeqs
+
+#=========================================================================
+compl_iupacdict = {'A':'T',
+                   'C':'G',
+                   'G':'C',
+                   'T':'A',
+                   'M':'K',
+                   'R':'Y',
+                   'W':'W',
+                   'S':'S',
+                   'Y':'R',
+                   'K':'M',
+                   'V':'B',
+                   'H':'D',
+                   'D':'H',
+                   'B':'V',
+                   'X':'X',
+                   'N':'N',
+                   '-':'-'}
+
+def compliment(motif, compl_iupacdict):
+    compl_motif = ""
+    for i in range(0,len(motif)):
+        letter = motif[i]
+        compl_motif = compl_motif + compl_iupacdict[letter]
+    return compl_motif
+
+def reverse(text):
+    return text[::-1]
+
+def revComp(seq):
+    revCompSeq = reverse(compliment(seq, compl_iupacdict))
+    return revCompSeq
+#=========================================================================
+
+def iupacList_2_regExList(motifList):
+    i = 0
+    while i < len(motifList):
+        motifList[i] = [motifList[i], iupac2regex(motifList[i])]
+        i += 1
+
+
+
+
+def iupac2regex(motif):
+
+    iupacdict = {'A':'A',
+                 'C':'C',
+                 'G':'G',
+                 'T':'T',
+                 'M':'[AC]',
+                 'R':'[AG]',
+                 'W':'[AT]',
+                 'S':'[CG]',
+                 'Y':'[CT]',
+                 'K':'[GT]',
+                 'V':'[ACG]',
+                 'H':'[ACT]',
+                 'D':'[AGT]',
+                 'B':'[CGT]',
+                 'X':'[ACGT]',
+                 'N':'[ACGT]'}
+
+    transl_motif = ""
+    for i in range(0,len(motif)):
+        letter = motif[i]
+        transl_motif = transl_motif + iupacdict[letter]
+    return transl_motif
