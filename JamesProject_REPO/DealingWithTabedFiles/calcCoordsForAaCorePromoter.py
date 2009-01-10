739ebf07bd4c40e7a2775dca5f057cf628140f73
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2009-01-10 19:54:50 +0000

    Massive Commit
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@24 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

--- JamesProject_REPO/DealingWithTabedFiles/calcCoordsForAaCorePromoter.py
+++ JamesProject_REPO/DealingWithTabedFiles/calcCoordsForAaCorePromoter.py
@@ -1,7 +1,7 @@
 
 
-inFile  = '/Users/biggus/Documents/James/Data/AedesCorePromoterWork/Aa_Ensbl49_AaegL1.1.NR.txt'
-outFile = '/Users/biggus/Documents/James/Data/AedesCorePromoterWork/Aa_Ensbl49_AaegL1.1.PlusMinus50coords.txt'
+inFile  = '/Users/biggus/Documents/James/Data/AedesCorePromoterWork/sourceCoords/Aa_Ensbl49_AaegL1.1.NR.txt'
+outFile = '/Users/biggus/Documents/James/Data/AedesCorePromoterWork/sourceCoords/Aa_Ensbl49_AaegL1.1.Plus50Minus100coords.txt'
 
 
 inFile = map(lambda line: line.strip().split('\t'), open(inFile, 'rU').readlines())
@@ -11,22 +11,30 @@ passedRecs = 0
 
 def calcCoords(line, passedRecs):
     if int(line[6]) == 1:
-        if int(line[7])-50 < 0:
-            passedRecs+=1
-            print "%s was too short." % (line[0])
-        else:            
-            return '%s\t%s\t%s\t%s\t%s\t%s\n' % (line[3],line[0],line[1],line[6],int(line[7])-50,int(line[7])+50)
+        return '%s\t%s\t%s\t%s\t%s\t%s' % (line[3],line[0],line[1],line[6],int(line[7])-100,int(line[7])+100)
     elif int(line[6]) == -1:
-        if int(line[8])-50 < 0:
-            passedRecs+=1
-            print "%s was too short." % (line[0])
-        else:
-            return '%s\t%s\t%s\t%s\t%s\t%s\n' % (line[3],line[0],line[1],line[6],int(line[8])-50,int(line[8])+50)
+        return '%s\t%s\t%s\t%s\t%s\t%s' % (line[3],line[0],line[1],line[6],int(line[8])-100,int(line[8])+100)
 
 coordList = []
 for line in inFile:
     coordList.append(calcCoords(line,passedRecs))
-    
-outFile = open(outFile,'w')
 
-outFile.writelines(coordList)
+# check for neg numbers in the coords and add trailing \n
+c = 0
+for i in range(0, len(coordList)):
+    coordList[i] = coordList[i].split('\t')
+    if int(coordList[i][4]) < 0:
+        print '%s was too short.' % (coordList[i][0])
+        coordList[i] = ''
+        c+=1
+    else:
+        coordList[i] = "\t".join(coordList[i])+'\n'
+
+outFile = open(outFile,'w')
+for each in coordList:
+    #print each
+    outFile.write(each)
+
+#outFile.writelines(coordList)
+
+print '--------\nDone.\n%s seqs were too short and were ommited.\n**see above**' % (c)
