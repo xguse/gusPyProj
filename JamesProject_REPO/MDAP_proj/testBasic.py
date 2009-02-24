a6c5c3aac4228c96329be0bed9753f4086620939
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2009-02-24 00:45:07 +0000

    Nothing to report
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@31 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

--- JamesProject_REPO/MDAP_proj/testBasic.py
+++ JamesProject_REPO/MDAP_proj/testBasic.py
@@ -2,9 +2,9 @@ from MD_wrappers import TamoWrap
 
 # TamoWrap -> def __init__(self, optionsObj, posArgs):
 
-optionsObj = {'kmerSize':7,'kmerRange':None}
-posArgs    = ['/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Anopheles/2KBupTSS_goodAffyAGAPsFastasOUT.masked.nr.fas','/Users/biggus/Documents/James/Data/ReClustering/kmedsPear33Clus50x_2/kmedsPear33Clus50x_2-2.genes.txt']
-outFile    = '/Users/biggus/Documents/James/Writings_Talks/Grants/09_Feb/PrelimData_Grant_Feb09/Clus2_7merSearch-0.01.txt'
+optionsObj = {'kmerSize':7,'kmerRange':'6,9'} # need to change the use of this to be how mortals think of numbers
+posArgs    = ['/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Anopheles/2KBupTSS_goodAffyAGAPsFastasOUT.masked.nr.fas','/Users/biggus/Documents/James/Data/DoubleBloodMeal/DBMclusterGenes.txt']
+outFile    = '/Users/biggus/Documents/James/Data/DoubleBloodMeal/DBMgenesKmerAnalysis.6-8mers.txt'
 
 tWrap = TamoWrap(optionsObj,posArgs)
 tWrap.go()
