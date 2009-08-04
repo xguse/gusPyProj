c02d45017d46f0e11426ecdf7e8323d9a1e5a6e3
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2009-08-04 19:20:28 +0000

    initial mosqData package commit
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@41 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

---
+++ JamesProject_REPO/defs/mosqData/seqInfo.py
@@ -0,0 +1,11 @@
+# Anopheles gambiae
+Ag_Good_2000Up_masked        = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Anopheles/2KBupTSS_goodAffyAGAPsFastasOUT.masked.nr.fas'
+
+# Aedes aegypti
+Aa_2000bpUp_softMasked       = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.softMasked.geneStrand.fas'
+Aa_2000bpUp_hardMasked       = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.fas'
+Aa_2000bpUp_hardMasked_shuf1 = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.shuffledSeqs.1.fas'
+Aa_2000bpUp_hardMasked_shuf2 = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.shuffledSeqs.2.fas'
+Aa_2000bpUp_hardMasked_shuf3 = '/'
+
+# Culex quinquefasciatus
