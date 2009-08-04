0cec3dd2dc8251f0efa6ff3a66d816f4502d4d34
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2009-08-04 19:41:51 +0000

    added AgNucFreqs
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@43 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

--- JamesProject_REPO/defs/mosqData/seqInfo.py
+++ JamesProject_REPO/defs/mosqData/seqInfo.py
@@ -1,11 +1,14 @@
+## unless otherwise stated the nucFreq values are derived from (total nucleotide number)/(total NON-N nucleotides)
+
+
 # Anopheles gambiae
-Ag_Good_2000Up_masked        = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Anopheles/2KBupTSS_goodAffyAGAPsFastasOUT.masked.nr.fas'
+Ag_Good_2000Up_masked_nucFreqs = {'A':0.277,'C':0.222,'G':0.222,'T':0.277}
 
 # Aedes aegypti
-Aa_2000bpUp_softMasked       = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.softMasked.geneStrand.fas'
-Aa_2000bpUp_hardMasked       = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.fas'
-Aa_2000bpUp_hardMasked_shuf1 = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.shuffledSeqs.1.fas'
-Aa_2000bpUp_hardMasked_shuf2 = '/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.shuffledSeqs.2.fas'
-Aa_2000bpUp_hardMasked_shuf3 = '/'
+Aa_2000bpUp_softMasked       = None #'/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.softMasked.geneStrand.fas'
+Aa_2000bpUp_hardMasked       = None #'/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.fas'
+Aa_2000bpUp_hardMasked_shuf1 = None #'/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.shuffledSeqs.1.fas'
+Aa_2000bpUp_hardMasked_shuf2 = None #'/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Aedes/aedes2KBupStreamTSS.hardMasked.geneStrand.shuffledSeqs.2.fas'
+Aa_2000bpUp_hardMasked_shuf3 = None #'/'
 
 # Culex quinquefasciatus
