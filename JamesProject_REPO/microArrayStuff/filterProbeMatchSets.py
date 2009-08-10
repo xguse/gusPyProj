106d39956c01f744aa47f91ccb229c20598ac31e
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2009-08-10 19:30:14 +0000

    First complete version
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@50 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

--- JamesProject_REPO/microArrayStuff/filterProbeMatchSets.py
+++ JamesProject_REPO/microArrayStuff/filterProbeMatchSets.py
@@ -10,10 +10,10 @@ single entity ~Gene.
 
 """
 # <userSettings>
-inFile     = ''
-outfile    = ''
+inFile     = '/Users/biggus/Documents/James/Data/MicroArrayPrep/Aedes/Probes/ProbeBlastResults/Aedes.agilent.probes.nr.blastn_low.AedesTxs.30sets.txt'
+outfile    = '/Users/biggus/Documents/James/Data/MicroArrayPrep/Aedes/Probes/ProbeBlastResults/Aedes.agilent.probes.nr.blastn_low.AedesTxs.30sets.atLeast10.toTheGene.txt'
 atLeastNum = 10
-onlyGenes  = None
+onlyGenes  = True
 # </userSettings>
 
 
@@ -44,5 +44,26 @@ def loadLines(filePath,onlyGenes=None):
 
 matchSets = loadLines(inFile,onlyGenes=onlyGenes)
 
+rtnProbes = []
 
+for probe in matchSets:
+    if onlyGenes:
+        assert probe[:-3] in matchSets[probe], \
+               "ERROR: Probe %s does not seem to have matched itself." % (probe)
+    else:
+        assert probe in matchSets[probe], \
+               "ERROR: Probe %s does not seem to have matched itself." % (probe)
+    
+    if len(matchSets[probe]) >= atLeastNum+1: # we will not count its match to itself
+        rtnProbes.append(probe)
+
+rtnProbes.sort()
+
+
+# write out filtered List:
+outfile = open(outfile, 'w')
+for probe in rtnProbes:
+    outfile.write('%s:\t%s\n' % (probe,'\t'.join(matchSets[probe])))
+    
+print 'Written %s sets with at least %s matches to the probe.' % (len(rtnProbes), atLeastNum)
 
