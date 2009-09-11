7408cf9d9bd879d1caa27c7d78f5f8b9afe66a6d
biggus <biggus@a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f>
2009-09-11 18:02:14 +0000

    Almost Final Version I think!
    2009-08-20 -- Wrote miRNA.countHitsInOrthos()
    
    2009-08-20 -- Tested miRNA.tallyHits(): seems to work as expected
    
    2009-08-20 -- Tested miRNA.countHitsInOrthos(): seems to work as expected
    
    2009-08-26 -- Finished miRNA.loadSeqs().
    
    2009-08-26 -- Finished miRNA.loadOrthos().
    
    2009-08-27 -- Changed miRNA.tallyHits() -> initialization of self.ctrlData[seedType] from [set()]*number.  This creates many refs to ONE set().  NOT good.  Did it the hard way now.
    
    
    git-svn-id: file:///Users/biggus/Documents/Programming/WingProjects/svn@53 a2b54c85-9f4f-4f1b-90b3-fafe6c4b0b9f

--- JamesProject_REPO/defs/miRNA_targeting.py
+++ JamesProject_REPO/defs/miRNA_targeting.py
@@ -1,7 +1,10 @@
+import numpy
 import re
+from TAMO.seq import Fasta
 import xpermutations
 import JamesDefs
 import bioDefs
+import mathDefs
 
 
 
@@ -19,9 +22,11 @@ _seedModels = {'m3_to_m8':[2,8],
               'm2_to_m8':[1,8],
               'A1_to_m8':[0,8,'A1'],}
 
+
+
 class miRNA:
     """
-    Class to represent miRNAs, including the versions of seed matches.
+    Class to represent seedMatches.
     """
     
     def __init__(self,miRNA,set4realMatches,orthoRelations=None,genomeTokens=None,name=None,numOfCtrls=15):
@@ -40,18 +45,21 @@ class miRNA:
         self.name           = name
         self.sourceSeq      = miRNA.upper()
         self.sourceSeqType  = None
-        self.genomes        = genomeTokens
+        ##self.genomes        = genomeTokens
         self.orthos         = orthoRelations
         self.matchVersions  = {}  # formerly self.seed
         self.matchData      = {}  # keys=str(seedType) : values=set([matchHits])
         self.ctrlData       = {}  # keys=str(seedType) : values=[set([ctrlVerOneHits]),set([ctrlVerTwoHits]), ... ]
-        self.matchCounts    = {}  # keys=str(seedType) : values=[countInAnyGenome,countIn1:1s, countIn1:1:1s]
-        self.ctrlCounts     = {}  # keys=str(seedType) : values=[[countInAnyGenome_1,countIn1:1s_1, countIn1:1:1s_1],[countInAnyGenome_2,countIn1:1s_2, countIn1:1:1s_2] ...]
+        self.matchCounts    = {}  # keys=str(seedType) : values=[countInNone,countInAnyGenome,countInAtLeast1:1s, countIn1:1:1s]
+        self.ctrlCounts     = {}  # keys=str(seedType) : values=[[countInNone_1, countInAnyGenome_1,countInAtLeast1:1s_1, countIn1:1:1s_1],[countInNone_2, countInAnyGenome_2,countInAtLeast1:1s_2, countIn1:1:1s_2] ...]
+        self.ctrlMeanStd    = {}  # keys=str(seedType) : values=[[countInNone-mean, countInNone-stdv], [countInAnyGenome-mean, countInAnyGenome-stdv],[countInAtLeast1:1s-mean, countInAtLeast1:1s-stdv], [countIn1:1:1s-mean, countIn1:1:1s-stdv]]
+        self.ctrlMedianStd  = {}  # keys=str(seedType) : values=[[countInNone-median, countInNone-stdv], [countInAnyGenome-median, countInAnyGenome-stdv],[countInAtLeast1:1s-median, countInAtLeast1:1s-stdv], [countIn1:1:1s-median, countIn1:1:1s-stdv]]
+        self.ctrlMedianMAD  = {}  # keys=str(seedType) : values=[[countInNone-median, countInNone-MAD], [countInAnyGenome-median, countInAnyGenome-MAD],[countInAtLeast1:1s-median, countInAtLeast1:1s-MAD], [countIn1:1:1s-median, countIn1:1:1s-MAD]]
+        self.seedScores     = {}  # keys=str(seedType) : values={keys=scoreType:values=zScoreNumber}
         
         self._loadmatchVersions(set4realMatches)
         
-        # Data storage variables are initialized during the counting,after
-        # the ctrls have been specified
+        # Structures of data storage variables are initialized in the def that fills them in.
         
 
 
@@ -123,7 +131,7 @@ class miRNA:
         for seedType in self.matchVersions:
             # If NO third index like here: [0,7,'A1']
             if len(_seedModels[seedType]) == 2:
-                # Select 15 random permutations of matchVersions[seedType] that are not in the 
+                # Select numOfCtrls random permutations of matchVersions[seedType] that are not in the 
                 # restrictedList.
                 matchPermList = [''.join(x) for x in xpermutations.xpermutations(list(self.matchVersions[seedType][0]))]
                 while len(self.matchVersions[seedType][1]) < numOfCtrls:
@@ -196,16 +204,17 @@ class miRNA:
             
     def tallyHits(self, flankingRegions):
         """
-        Takes a dict of flanking DNA seqs.  For each seq, it updates the counts for each real
-        matchVersion and each permutation of the ctrls for each matchVersion in self.matchData,
+        Takes a dict of flanking DNA seqs.  For each seq, it updates the genes containing each
+        real matchVersion and each permutation of the ctrls for each matchVersion in self.matchData,
         and self.ctrlData.
         """
-## ! ## ! ## ! ## !  ##  MUST STILL RUN TEST FOR THIS FUNCTION  ## ! ## ! ## ! ## ! ##
         
         # Initialize data variables
         for seedType in _seedModels:
             self.matchData[seedType] = set()
-            self.ctrlData[seedType]  = [set()]*len(self.matchVersions[seedType][1])
+            self.ctrlData[seedType]  = []
+            for i in range(len(self.matchVersions[seedType][1])):
+                self.ctrlData[seedType].append(set())
         
         # Cycle through flanking regions
         for gene in flankingRegions:
@@ -219,14 +228,226 @@ class miRNA:
                     if flankingRegions[gene].find(self.matchVersions[seedType][1][i]) >= 0:
                         self.ctrlData[seedType][i].add(gene)
 
+    def countHitsInOrthos(self):
+        """
+        Uses results of miRNA.tallyHits() and self.orthos to count how many genes the miRNA seed
+        hits in at least one genome, in at least two orthologs, and in all three orthologs.
+        """
+        # make sure we have tallied the hits already.
+        assert self.matchData and self.ctrlData, \
+               'ERROR:  It looks like we have not tallied the hits yet. Call miRNA.tallyHits() first.'
+              
+        # Initialize self.matchCounts/self.ctrlCounts
+        for seedType in _seedModels:
+            self.matchCounts[seedType] = [0,0,0,0]
+            self.ctrlCounts[seedType]  = [[0]*4 for i in range(len(self.matchVersions[seedType][1]))]
+        # Cycle through self.orthos 
+        for orthoSet in self.orthos:
+            assert len(orthoSet) == 3,\
+                   'ERROR: It seems len(%s) != 3.'
+            # Query the matcheData and ctrlData for hits in orthoSet
+            for seedType in _seedModels:
+                genesInMatchD = 0
+                genesInCtrlD  = [0]*len(self.matchVersions[seedType][1])
+                # Count how many genes in each orthoSet were hit by the respective seedTypes
+                for gene in orthoSet:
+                    if gene in self.matchData[seedType]: genesInMatchD += 1
+                    for i in range(len(self.ctrlData[seedType])):
+                        if gene in self.ctrlData[seedType][i]: genesInCtrlD[i] += 1
+                        
+                # Update self.matchData based on how many hits the orthoSet got for seedType
+                if genesInMatchD == 0:
+                    self.matchCounts[seedType][0] += 3
+                elif genesInMatchD == 1:
+                    self.matchCounts[seedType][0] += 2
+                    self.matchCounts[seedType][1] += 1
+                elif genesInMatchD == 2:
+                    self.matchCounts[seedType][0] += 1
+                    self.matchCounts[seedType][1] += 2
+                    self.matchCounts[seedType][2] += 1
+                elif genesInMatchD == 3:
+                    self.matchCounts[seedType][1] += 3
+                    self.matchCounts[seedType][2] += 3
+                    self.matchCounts[seedType][3] += 1
+                # Update self.ctrlData based on how many hits the orthoSet got in each ctrl for seedType
+                for i in range(len(self.ctrlData[seedType])):
+                    if genesInCtrlD[i] == 0:
+                        self.ctrlCounts[seedType][i][0] += 3
+                    elif genesInCtrlD[i] == 1: 
+                        self.ctrlCounts[seedType][i][1] += 1
+                    elif genesInCtrlD[i] == 2:
+                        self.ctrlCounts[seedType][i][1] += 2
+                        self.ctrlCounts[seedType][i][2] += 1
+                    elif genesInCtrlD[i] == 3:
+                        self.ctrlCounts[seedType][i][1] += 3
+                        self.ctrlCounts[seedType][i][2] += 3
+                        self.ctrlCounts[seedType][i][3] += 1
+                        
+    def calcCtrlMeanStDv(self):
+        """
+        Iterate through the self.ctrlCount data and calculate the mean and stdDev for each
+        seedType permutation set.
+        """
+## ! ## ## ! ## ## ! ##  NEEDS TO BE TESTED!!!  ## ! ## ## ! ## ## ! ## 
+        # Make sure we have already run self.countHitsInOrthos()
+        assert self.matchCounts and self.ctrlCounts, \
+               'ERROR:  It looks like we have not called countHitsInOrthos().'
+        
+        # Initialize data structure
+        for seedType in _seedModels:
+            self.ctrlMeanStd[seedType]  = [[None,None],[None,None],[None,None],[None,None]] # SEE self.ctrlStats def in __init__ for format definition
+        
+            
+        # Iterate through self.ctrlCounts
+        for seedType in self.ctrlCounts.keys():
+            # Use numpy to record mean and StDv for each countCategory in self.ctrlCounts[seedType]
+            for i in range(len(self.ctrlMeanStd[seedType])):
+                meanStDv, mean = mathDefs.stdDv([x[i] for x in self.ctrlCounts[seedType]],kind='mean',df=1)
+                self.ctrlMeanStd[seedType][i][0] = mean
+                self.ctrlMeanStd[seedType][i][1] = meanStDv
+                
+    def calcCtrlMedianStDv(self):
+        """
+        Iterate through the self.ctrlCount data and calculate the MEDIAN and stdDev from the
+        MEDIAN for each seedType permutation set.
+        """
+## ! ## ## ! ## ## ! ##  NEEDS TO BE TESTED!!!  ## ! ## ## ! ## ## ! ## 
+        # Make sure we have already run self.countHitsInOrthos()
+        assert self.matchCounts and self.ctrlCounts, \
+               'ERROR:  It looks like we have not called countHitsInOrthos().'
+        
+        # Initialize data structure
+        for seedType in _seedModels:
+            self.ctrlMedianStd[seedType]  = [[None,None],[None,None],[None,None],[None,None]] # SEE self.ctrlStats def in __init__ for format definition
+        
+        # Iterate through self.ctrlCounts
+        for seedType in self.ctrlCounts:
+            # Calc median and  medianStDv for each countCategory in self.ctrlCounts[seedType]
+            for i in range(len(self.ctrlMedianStd[seedType])):
+                medStDv, median = mathDefs.stdDv([x[i] for x in self.ctrlCounts[seedType]],kind='median',df=1)
+                self.ctrlMedianStd[seedType][i][0] = median
+                self.ctrlMedianStd[seedType][i][1] = medStDv
+                
+    def calcCtrlMedianMAD(self):
+        """
+        Iterate through the self.ctrlCount data and calculate the MEDIAN and the 
+        Median Absolute Deviations for each seedType permutation set.
+        """
+## ! ## ## ! ## ## ! ##  NEEDS TO BE TESTED!!!  ## ! ## ## ! ## ## ! ## 
+        # Make sure we have already run self.countHitsInOrthos()
+        assert self.matchCounts and self.ctrlCounts, \
+               'ERROR:  It looks like we have not called countHitsInOrthos().'
+        
+        # Initialize data structure
+        for seedType in _seedModels:
+            self.ctrlMedianMAD[seedType]  = [[None,None],[None,None],[None,None],[None,None]] # SEE self.ctrlStats def in __init__ for format definition
+        
+        # Iterate through self.ctrlCounts
+        for seedType in self.ctrlCounts:
+            # Calc median and MAD for each countCategory in self.ctrlCounts[seedType]
+            for i in range(len(self.ctrlMedianMAD[seedType])):
+                MAD,median = mathDefs.medianAbsDev([x[i] for x in self.ctrlCounts[seedType]])
+                self.ctrlMedianMAD[seedType][i][0] = median
+                self.ctrlMedianMAD[seedType][i][1] = MAD
+                
+    def scoreSeedType(self, seedType, howManyOrthos, metric='medianStDv'):
+        """
+        scoreSeedType(self, seedType, howManyOrthos, metric='medianStDv')
+        Returns the respective type of z-score.
+        """
+        metrics = {'meanStDv':self.ctrlMeanStd,
+                   'medianStDv':self.ctrlMedianStd,
+                   'medianMAD':self.ctrlMedianMAD,}
+        
+        
+        assert seedType in _seedModels,\
+               '\n\nERROR: Valid values of seedType include %s.' % (_seedModels)
+        assert 0<=howManyOrthos<=3, \
+               "\n\nERROR: howManyOrthos must be >= 0 and <= 3."
+        assert metric in metrics,\
+               "\n\nERROR: Valid values of metric include %s." % (metrics.keys())
+        assert metrics[metric], \
+               "\n\nERROR: %s has not been calculated yet." % (metric)
+        
+        real       = self.matchCounts[seedType][howManyOrthos]
+        ctrlCenter = metrics[metric][seedType][howManyOrthos][0]
+        ctrlSpread = metrics[metric][seedType][howManyOrthos][1]
+        
+        return (float(real)-ctrlCenter)/ctrlSpread
+        
+    def test(self):
+        print 'it works!'
+
+
 
 #-#  Global defs  #-#
-def learnOrthoRelations():
-    pass
-def learnGenomes():
-    pass
+def loadSeqs(fastaPathList):
+    """
+    Takes list of paths.  Returns single dict full of seqs found in the files.
+    Converts softMasking to hard.
+    """
+    rDict = {}
     
+    for path in fastaPathList:
+        rDict.update(Fasta.load(path))
+    
+    bioDefs.softMaskDict2HardMask(rDict)
+    return rDict
 
+def loadOrthos(orthoPath, seqDict):
+    """
+    Takes path to ortho relation file.
+    Returns list lof lists: [[orthoNameA, orthoNameB, orthoNameC]].
+    Filters out any orthoLists that contain a gene that is missing from seqDict.
+    """
+    
+    seqDictKeys = set(seqDict.keys())
+    rList = []
+    orthoPath = open(orthoPath, 'rU')
+    originalCount = 0
+    while 1:
+        line = orthoPath.readline().strip()
+        if not line: break
+        originalCount+=1
+        try:
+            line = eval(line)
+            assert type(line) == type([]), 'loadOrthos ERROR: type(line) != type([])'
+            # Filter
+            passed = True
+            for gene in line:
+                if gene not in seqDictKeys:
+                    passed = False
+                    break
+            if passed:
+                rList.append(line)
+        except:
+            rList.append(line.split('\t'))
+    
+    print '%s of %s original orthoRelations were left after filtering.' % (len(rList), originalCount) 
+    return rList
+
+def loadMiRNAs(miRNA_Path):
+    """
+    Takes fasta file of mature miRNAs.
+    Returns dict.
+    """
+    
+    return Fasta.load(miRNA_Path)
+
+def filterOrthoSeqs(seqDict, orthoRelations):
+    """
+    Returns a dict with only those seqs that are named in orthoRelations.
+    """
+    flatList = sum([l for l in orthoRelations],[])
+    
+    rDict = {}
+    
+    for gene in flatList:
+        rDict[gene] = seqDict[gene]
+        
+    return rDict
+    
+    
     
 
 
@@ -242,7 +463,13 @@ changeLog = """2009-05-25 -- script created.
 2009-08-13 --  altered var names of class SingleSeqHits
 2009-08-14 -- declassified SeedModels in favor of a global variable '_seedModels' (the class had no methods and I did not plan for it to have any)
 2009-08-15 -- Did away with classes SingleHits and OrthoHits, moved data storage into miRNA
-2009-08-15 -- Wrote miRNA.tallyHits
+2009-08-15 -- Wrote miRNA.tallyHits()
+2009-08-20 -- Wrote miRNA.countHitsInOrthos()
+2009-08-20 -- Tested miRNA.tallyHits(): seems to work as expected
+2009-08-20 -- Tested miRNA.countHitsInOrthos(): seems to work as expected
+2009-08-26 -- Finished miRNA.loadSeqs().
+2009-08-26 -- Finished miRNA.loadOrthos().
+2009-08-27 -- Changed miRNA.tallyHits() -> initialization of self.ctrlData[seedType] from [set()]*number.  This creates many refs to ONE set().  NOT good.  Did it the hard way now.
 """
 
 notes = """2009-05-25 -- Recording hits to each type of seed:
