95574982b28b9217a3b556f50e59038aa2c2033f
Augustine Dunn <wadunn83@gmail.com>
2009-10-30 12:28:43 -0700

    Revert "moved goEnrichment() to miRNA_targeting.py"
    This reverts commit 7281149cc28265b3a5c26ce3c08608105a1dbfcb.

--- gusPyCode/defs/miRNA_targeting.py
+++ gusPyCode/defs/miRNA_targeting.py
@@ -5,7 +5,6 @@ from gusPyCode.defs import xpermutations
 from gusPyCode.defs import JamesDefs
 from gusPyCode.defs import bioDefs
 from gusPyCode.defs import mathDefs
-from gusPyCode.defs.statsDefs import hypergeoP
 from pprint import pprint
 
 
@@ -746,31 +745,8 @@ def getTokenFromNames(geneNames):
         
     return sorted(list(toks))
     
-
-
-
-
-def goEnrichment(miR_targets,GOgenes,popSize):
-    """
-    goEnrichment(miR_targets,GOgenes,popSize):
-    miR_targets = set(predictedGenes)
-    GOgenes     = set(genesInGOgroup)
-    popSize     = int(numberOfGenesConsideredAsPopulation)
     
-    Returns cumulative hypergeometric enrichment P-value.
-    """
-    #n = # of positives in population
-    #i = # of positives in sample
-    #m = # of negatives in population
-    #N = sample size
-    #P(x=i) = (choose(n,i)choose(m,N-i))/choose(n+m,N)
-    #For more details -> http://mathworld.wolfram.com/HypergeometricDistribution.html
-    n = len(GOgenes)
-    i = len(GOgenes.intersection(miR_targets))
-    m = popSize-len(GOgenes)
-    N = len(miR_targets)
     
-    return sum([hypergeoP(n,x,m,N) for x in range(i,N+1)])
 
 
     
