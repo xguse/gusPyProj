d48084edce43ed42f17277535204004202c76834
Augustine Dunn <wadunn83@gmail.com>
2009-11-03 19:37:20 -0800

    fixed gusPyCode paths

--- gusPyCode/!simpleScripts/geneList2Fasta.py
+++ gusPyCode/!simpleScripts/geneList2Fasta.py
@@ -1,13 +1,14 @@
 from TAMO.seq import Fasta
-from bioDefs import geneList2FastaDict
-import promoterSeqPaths
+from gusPyCode.defs.bioDefs import geneList2FastaDict
+from gusPyCode.defs.mosqData import promoterSeqPaths
 
 
-geneList = map(lambda l: l.strip(), open('/Users/biggus/Documents/James/Collaborations/Campbell/data/CCupAt4Days.genes.txt', 'rU'))
+geneList = map(lambda l: l.strip(), \
+               open('/Users/biggus/Documents/James/Collaborations/Campbell/data/CCupAt4Days.gte2x.genes.txt', 'rU'))
 
-sourceFasta = promoterSeqPaths.Aa_2000bpUp_hardMasked_shuf3
+sourceFasta = promoterSeqPaths.Aa_2000bpUp_hardMasked_shuf1
 
-oFile = '/Users/biggus/Documents/James/Collaborations/Campbell/data/CCupAt4Days.masked.shuffled.3.fas'
+oFile = '/Users/biggus/Documents/James/Collaborations/Campbell/data/CCupAt4Days.gte2x.masked.shuffled.1.fas'
 
 
 
