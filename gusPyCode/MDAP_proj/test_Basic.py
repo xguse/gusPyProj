426a41a4f956fa05151d05a3c4301d32cdef213e
Augustine Dunn <wadunn83@gmail.com>
2010-05-03 08:30:29 -0700

    MDAP_proj.test_Basic: -added opts parsing

--- gusPyCode/MDAP_proj/test_Basic.py
+++ gusPyCode/MDAP_proj/test_Basic.py
@@ -1,10 +1,42 @@
+import optparse
 from gusPyCode.MDAP_proj.MD_wrappers import TamoWrap
 
 # TamoWrap -> def __init__(self, optionsObj, posArgs):
 
-optionsObj = {'kmerSize':7,'kmerRange':'6,10'} # need to change the use of this to be how mortals think of numbers
-posArgs    = ['/Users/biggus/Documents/James/Data/2KB/2kb_Sequence/2kb_Anopheles/2KBupTSS_goodAffyAGAPsFastasOUT.masked.nr.seqsShuffled.fas','/Users/biggus/Documents/James/Data/ReClustering/kmedsPear33Clus50x_2/Clus2_247genes.genes.txt']
-outFile    = '/Users/biggus/Documents/James/Data/ReClustering/kmedsPear33Clus50x_2/Clus2_247genes.6-8mers.txt'
+#+++++++++++ File Parseing Etc +++++++++++    
+usage = """python %prog [options]  scopeOut.xml [scopeOut.xml ...]"""
+parser = optparse.OptionParser(usage)
+
+parser.add_option('--kSize',dest="ksize",type="int", default=7, 
+                  help="""k-mer size. (default=%default)""")
+parser.add_option('--kRange', dest="krange", type='string',default='6,10',
+                  help="""k-mer range (python range-type). (default=%default)""")
+parser.add_option('-o', dest="out", type='string',default=None,
+                  help="""REQUIRED: File name for output (default=%default)""")
+parser.add_option('--fas', dest="fasta", type='str',default=None,
+                  help="""REQUIRED: Fasta file of ALL regulatory sequences. (default=%default)""")
+parser.add_option('--regs', dest="regs", type='str',default=None,
+                  help="""REQUIRED: File with list of regulated genes. (default=%default)""")
+
+
+(opts, args) = parser.parse_args()
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    exit(0)
+if (not opts.fas) or (not opts.regs) or (not opts.out):
+    parser.print_help()
+    print '\n'
+    raise Exception, 'ERROR: Missing a required input.'
+
+
+
+
+
+
+optionsObj = {'kmerSize':opts.ksize,'kmerRange':opts.krange} # need to change the use of this to be how mortals think of numbers
+posArgs    = [opts.fasta,opts.regs]
+outFile    = opts.out
 
 tWrap = TamoWrap(optionsObj,posArgs)
 tWrap.go()
