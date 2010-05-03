a86610a54b01abacdbb107a3e5a2eda7ea9366b5
Augustine Dunn <wadunn83@gmail.com>
2010-05-03 08:32:10 -0700

    prototypes: norm2ReadSets

--- gusPyCode/defs/prototypes.py
+++ gusPyCode/defs/prototypes.py
@@ -1,57 +1,31 @@
-aCnts = {'1':{3:4,
-              4:3,
-              5:5,
-              6:7,
-              10:10,
-              11:9,}}
-
-def _writeWiggle(trackName, trackDescription, allCounts, wigOut, win=1):
-    """Writes the allCounts dictionary out to a wiggle file."""
-    #wigFile = open(wigOut, "w")
-    #wigFile.write("track type=wiggle_0 name='%s' description='%s' visibility=2\n" % (trackName,
-                                                                                     #trackDescription))
-    print "track type=wiggle_0 name='%s' description='%s' visibility=2\n" % (trackName,
-                                                                             trackDescription)
-    assert type(win) == type(1), \
-           'Error: win must be integer. You gave: %s' % (win)
-
-    for name in allCounts.keys():
-        start = 0
-        end = max(allCounts[name].keys())
-
-        #wigFile.write("fixedStep chrom=%s start=%s step=%s span=%s\n" % (name, start+1, win, win))
-        print "fixedStep chrom=%s start=%s step=%s span=%s\n" % (name, start+1, win, win)
-        i=0
-        #for i in range(start, end):
-        while i <= end:
-            winCounts = []
-            for pos in range(i,i+win):
-                if allCounts[name].has_key(pos):
-                    winCounts.append(allCounts[name][pos])
-                else:
-                    winCounts.append(0)
-            winAvg = sum(winCounts)/float(len(winCounts))
-            #wigFile.write("%.4f\n" % (winAvg))
-            print "%.4f\n" % (winAvg)
-            i+=win
-
-    #wigFile.close()
-
+def norm2ReadSets(list1, list2):
+    """Takes two lists -> Returns two Normalized Lists.
+    Normalizes Read counts in each index based
+    on the list with the smallest total (this 
+    list is not changed). Indexes in list with 
+    larger total are multiplied by 
+    (float(sum(smaller))/sum(larger)) to scale the
+    read counts to those expected if both total reads
+    equaled sum(smaller)."""
+    if not len(list1) == len(list2):
+        raise Exception, "ERROR: len(list1) != len(list2)."
     
-def _writeWiggleVar(trackName, trackDescription, allCounts, wigOut):
-    """Writes the allCounts dictionary out to a wiggle file."""
-    wigFile = open(wigOut, "w")
-    wigFile.write("track type=wiggle_0 name='%s' description='%s' visibility=2\n" % (trackName,
-                                                                                     trackDescription))
-
-    for name in allCounts.keys():
-        start = 0
-        end = max(allCounts[name].keys())
-
-        wigFile.write("variableStep chrom=%s span=1\n" % (name))
-
-        for pos in sorted(allCounts[name].keys()):
-            wigFile.write("%s\t%s\n" % (pos,allCounts[name][pos]))
-
-
-    wigFile.close()
+    # ++ float-ify lists ++
+    for i in range(len(list1)):
+        list1[i] = float(list1[i])
+        list2[i] = float(list2[i]) 
+    
+    totL1,totL2 = sum(list1),sum(list2)
+    
+    normd1,normd2 = [],[]
+    
+    if totL1 < totL2:
+        for i in range(len(list1)):
+            normd1.append(list1[i])
+            normd2.append(list2[i]*(totL1/totL2))
+    else:
+        for i in range(len(list1)):
+            normd2.append(list2[i])
+            normd1.append(list1[i]*(totL2/totL1))
+    
+    return normd1,normd2
