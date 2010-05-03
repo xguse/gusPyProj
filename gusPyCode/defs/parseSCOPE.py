195fc220cca330fd68925e512cfec7240d41764f
Augustine Dunn <wadunn83@gmail.com>
2010-05-03 08:31:22 -0700

    parseSCOPE: -added toXMS capabilities

--- gusPyCode/defs/parseSCOPE.py
+++ gusPyCode/defs/parseSCOPE.py
@@ -4,10 +4,11 @@ from collections import namedtuple
 import pprint
 import os
 
-os.chdir('/Users/biggus/Documents/James/Collaborations/Campbell/scopeOut/')
+#os.chdir('/Users/biggus/Documents/James/Collaborations/Campbell/scopeOut/')
 # Basic Idea:
 # Parse scopeXMLoutput into motif objects filterable by rank, algorithm, or genes conating the motif,
 # and capable of outputing a SeqFeature(FeatureLocation()) passible to GenomeDiagram.
+
 
 
 
@@ -38,11 +39,54 @@ class SCOPEmotifResult(object):
         for base in xmlMotif.pwm:
             self.pwm[base.name.upper()] = tuple([int(x[2][0]) for x in base._children])
         
-    
+    def toXMSmotif(self):
+        """Return string representing the XMS motif element for
+        this motif.
+        """
+        ltr2wrd = {'A':'adenine',
+                   'C':'cytosine',
+                   'G':'guanine',
+                   'T':'thymine'}
+        
+        # ++ initialize xms motif string ++
+        xMtf = '<motif>\n\t<name>%s_%.3f</name>\n\t\t<weightmatrix alphabet="DNA" columns="%s">\n' %\
+             (self.consensus,float(self.sigvalue),len(self.pwm['A']))
+        # ++ create and add each column's data ++
+        def getFreq(col,base):
+            """get fractional weight for a base at given column."""
+            counts = [float(self.pwm['A'][col]),
+                      float(self.pwm['C'][col]),
+                      float(self.pwm['G'][col]),
+                      float(self.pwm['T'][col])]
+            tot = sum(counts)
+            return float(self.pwm[base][col])/tot
+                        
+        for i in range(len(self.pwm['A'])):
+            xMtf += '\t\t\t<column pos="%s">\n' % (i)
+            for base in sorted(self.pwm.keys()):
+                freq = getFreq(i,base)
+                xMtf += '\t\t\t\t<weight symbol="%s">%.4f</weight>\n' % \
+                     (ltr2wrd[base],freq)
+            xMtf += "\t\t\t</column>\n"
+        xMtf += '\t\t</weightmatrix>\n'
+        xMtf += '\t\t<threshold>0</threshold>\n' # for now we'll just use null threshold
+        xMtf += '\t\t<prop>\n\t\t\t<key>consensus</key>\n\t\t\t<value>%s</value></prop>\n' % \
+             (self.consensus)
+        xMtf += '\t\t<prop>\n\t\t\t<key>sigvalue</key>\n\t\t\t<value>%s</value></prop>\n' % \
+             (self.sigvalue)
+        xMtf += '\t\t<prop>\n\t\t\t<key>sigValRank</key>\n\t\t\t<value>%s</value></prop>\n' % \
+             (self.sigValRank)
+        xMtf += '\t\t<prop>\n\t\t\t<key>algorithm</key>\n\t\t\t<value>%s</value></prop>\n' % \
+             (self.algorithm)
+        xMtf += '</motif>\n'
+        
+        return xMtf
+        
+        
     def toTAMO(self,bkFreqs=None):
         """Return TAMO motif Object from self.pwm
         """
-        pass
+        
     def toTRANSFACstring(self):
         """Returns printable motif representation in TRANSFAC format sutible for use in MAST.
         """
@@ -76,5 +120,16 @@ def parseScopeXML(pathToXML):
     
     return rDict
     
-    
+def toXMSfile(mtfDict,outPath):
+    """Take a scopeMotif dict and write out the motifs in XMS format."""
+    dKeys = sorted(mtfDict.keys(), key=lambda x: float(mtfDict[x].sigvalue))
+    dKeys.reverse()
+    oFile = open(outPath, 'w')
+    oFile.write('<motifset>\n')
+    for k in dKeys:
+        oFile.write('%s' % (mtfDict[k].toXMSmotif()))
+    oFile.write('</motifset>\n')
+    oFile.close()
+        
+
 None
