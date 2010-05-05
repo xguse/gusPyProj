294f5e934afcdf404f487f02f8bee6106e52356a
Augustine Dunn <wadunn83@gmail.com>
2010-05-05 07:51:32 -0700

    MDAP_proj.test_Basic: -added AND implemented option 'trim'

--- gusPyCode/MDAP_proj/test_Basic.py
+++ gusPyCode/MDAP_proj/test_Basic.py
@@ -1,5 +1,6 @@
 import optparse
 import sys
+from warnings import warn
 from gusPyCode.MDAP_proj.MD_wrappers import TamoWrap
 
 # TamoWrap -> def __init__(self, optionsObj, posArgs):
@@ -18,6 +19,9 @@ parser.add_option('--fas', dest="fasta", type='str',default=None,
                   help="""REQUIRED: Fasta file of ALL regulatory sequences. (default=%default)""")
 parser.add_option('--regs', dest="regs", type='str',default=None,
                   help="""REQUIRED: File with list of regulated genes. (default=%default)""")
+parser.add_option('--trim', dest='trim', type='str',default=None, \
+				  help="""Define substring of all sequences to be used.
+                  Exp: "-2000,None" -> last 2000 bp of all sequences. (default=%default)""")
 
 
 (opts, args) = parser.parse_args()
@@ -29,7 +33,17 @@ if (not opts.fasta) or (not opts.regs) or (not opts.out):
     parser.print_help()
     print '\n'
     raise Exception, 'ERROR: Missing a required input.'
-
+if opts.trim:
+    opts.trim = opts.trim.split(',')
+    if opts.trim[0] == 'None':
+        opts.trim[0] = None
+    else:
+        opts.trim[0] = int(opts.trim[0])
+        
+    if opts.trim[1] == 'None':
+        opts.trim[1] = None
+    else:
+        opts.trim[1] = int(opts.trim[1])
 
 
 
@@ -40,6 +54,19 @@ posArgs    = [opts.fasta,opts.regs]
 outFile    = opts.out
 
 tWrap = TamoWrap(optionsObj,posArgs)
+
+if opts.trim:
+    shorties = 0
+    for i in tWrap.allSeqs.probes:
+        oLen = len(tWrap.allSeqs.probes[i])
+        tWrap.allSeqs.probes[i] = tWrap.allSeqs.probes[i][opts.trim[0]:opts.trim[1]]
+        nLen = len(tWrap.allSeqs.probes[i])
+        #print 'old:%s, new:%s' % (oLen,nLen)
+        if not nLen < oLen:
+            shorties+=1
+if shorties/float(len(tWrap.allSeqs.probes)) > 0.25:
+    warn("""WARNING: more than 1/4 of the total sequences were shorter or equal to the length of the requested substring.""")
+tWrap.linkedSeqs_seqs = tWrap.allSeqs.seqs_from_ids(tWrap.linkedSeqs_ids)
 tWrap.go()
 
 #for l in tWrap.toFile:
